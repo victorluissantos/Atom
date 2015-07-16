2e085d5426fde5c502d29118bc84f4e959e0a5a4
Victor Santos <victorluissantos@live.com>
2015-07-15 21:45:48 -0300

    telas final

--- application/controllers/Usuario.php
+++ application/controllers/Usuario.php
@@ -3,22 +3,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Usuario extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->data['titulo'] = "Login !";
+	}
+
 	public function index()
+	{
+		$this->data['mensagem'] = "Login de acesso !";
+
+
+		$this->load->view('login', $this->data);
+	}
+	
+	public function home()
+	{
+		$this->load->view('login');
+	}
+
+	public function verifica_login()
 	{
 		$this->load->view('login');
 	}
--- application/views/usuario.php
+++ application/views/usuario.php
@@ -1,1 +1,5 @@
-usuario.php
+<title><?php echo $titulo; ?></title>
+<?php
+    echo "<link rel='stylesheet' href='".base_url('assets/bootstrap/css/bootstrap.min.css')."'>";
+    echo "<script src='".base_url('assets/bootstrap/js/bootstrap.min.js')."' type='text/javascript' language='javascript'>";
+?>
